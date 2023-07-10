5d8f9081234f44c453f8d6e745c3531a4dc79e7f
BahruDa <bahrudari68@gmail.com>
2023-07-10 13:31:49 +0300

    initSDL.c

--- src/initSDL.c
+++ src/initSDL.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <SDL2/SDL.h>
-
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#include "maze.h"
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -21,43 +16,20 @@ bool initSDL(void)
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         success = false;
     }
-    else
+
+    window = SDL_CreateWindow("Maze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
     {
-        window = SDL_CreateWindow("Maze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            success = false;
-        }
-        else
-        {
-            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-            if (renderer == NULL)
-            {
-                printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-                success = false;
-            }
-        }
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        success = false;
     }
 
     return success;
 }
-
-int main(void)
-{
-    if (initSDL())
-    {
-        // Initialization successful, proceed with your code
-    }
-    else
-    {
-        // Initialization failed, handle the error
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
-}
-
